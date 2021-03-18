c5a9af817ae3c49abfabf50930c1f22bff0a1907
daotiennamhl <43399562+daotiennamhl@users.noreply.github.com>
2021-03-18 14:53:56 +0700

    implement owner home page; (#41)

--- repositories/InnRepository.ts
+++ repositories/InnRepository.ts
@@ -5,33 +5,8 @@ import { Endpoint } from '~/constants/app.endpoint'
 
 
 export default class InnRepository {
-  public static getInnDetailByOwner(owner_id: unknown): Promise<any> {
-    const data = {
-      id : 5,
-      name : "Nhà trọ Minh Anh",
-      owner_id : 3,
-      water_price : 3500,
-      electric_price : 3500,
-      wifi_price: 100000,
-      description : "lorem ipsum dolor sit amet consectetur adipiscing elit. aliquam tincidunt elementum sem non luctus",
-      address :  "26 Võng thị, Phường Bưởi, Quận Tây Hồ, Hà Nội",
-      location : "48.19976  16.45572",
-      status : 1,
-      features: [1, 3, 5, 7, 9, 11, 13, 15],
-      close_time: "22h30",
-      open_time: "5h30",
-      imgs: [
-        '/imgs/anh_room.jpg',
-        '/imgs/anh_room.jpg',
-        '/imgs/anh_room.jpg',
-        '/imgs/anh_room.jpg',
-        '/imgs/anh_room.jpg',
-      ],
-   }
-
-    return new Promise(function (resolve) {
-      setTimeout(resolve.bind(null, {data}), 1000)
-    })
+  public static getInnDetailByOwner(): Promise<any> {
+    return authenticatedService.get(`${Endpoint.Owner}/inn/detail`)
   }
 
   public static createInn(data :any): Promise<AxiosResponse<any>> {
