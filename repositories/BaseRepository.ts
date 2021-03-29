36abdf0b8c4ec18e0cdff3e604e95bdd25d55edc
Do Duc Duy <doducduy.vietnam@gmail.com>
2021-03-29 19:31:37 +0700

    reset token axios

--- repositories/BaseRepository.ts
+++ repositories/BaseRepository.ts
@@ -2,15 +2,19 @@ import axios from 'axios';
 import { getTokenCookie } from '@/utils/cookies'
 import { COOKIES } from '@/constants/app.constant'
 
-export const unauthenticatedService = axios.create({
-    baseURL: `${process.env.BASE_API}/api`
-})
+const createAxiosInstance = () => {
+    return axios.create({
+        baseURL: `${process.env.BASE_API}/api`
+    })
+}
 
-export const authenticatedService = axios.create({
-    baseURL: `${process.env.BASE_API}/api`
-})
+export const unauthenticatedService = createAxiosInstance()
+
+
+export let authenticatedService = createAxiosInstance()
 
 export const setToken = (token: any) => {
+    authenticatedService = createAxiosInstance()
     authenticatedService.interceptors.request.use(
         (config) => {
             config.headers.Authorization = `Bearer ${token}`
